761590f4ad03a07ce6ddbb8d52891e518bd4715a
yukimasda <yuki.masuda@steamship.co.jp>
2025-01-14 13:41:58 +0900

    Nuxt4 first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }
+
+  // Nuxt4 互換モード
+  future: {
+    compatibilityVersion: 4,
+  },
+})
